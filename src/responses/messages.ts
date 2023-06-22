a3caee535b963eadc5fd568641502171e262dc21
Ashot Karapetyan <ashot.karapetyan.y@gmail.com>
2023-06-22 22:02:39 +0400

    Change objects to enums

--- src/responses/messages.ts
+++ src/responses/messages.ts
@@ -1,6 +1,6 @@
-export const ERROR_MESSAGES = {
-  ENDPOINT_NOT_FOUND: 'Endpoint not found',
-  DIRECTORY_FIELD_REQUIRED: 'Directory field required',
-  INTERNAL_SERVER_ERROR: 'Internal server error',
-  FILE_DOES_NOT_EXISTS: 'The specified file does not exist',
-};
+export enum ERROR_MESSAGES {
+  ENDPOINT_NOT_FOUND = 'Endpoint not found',
+  DIRECTORY_FIELD_REQUIRED = 'Directory field required',
+  INTERNAL_SERVER_ERROR = 'Internal server error',
+  FILE_DOES_NOT_EXISTS = 'The specified file does not exist',
+}
